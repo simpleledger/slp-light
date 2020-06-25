c3b8522077f0c509c027f100494fa26d0e335b92
Andreas Larsson <andreas.larsson@bitcoin.com>
2020-06-25 10:09:50 +0900

    Remove bitbox call

--- tests/RealTxMakerTest.ts
+++ tests/RealTxMakerTest.ts
@@ -7,7 +7,7 @@ test("Successfully send a tx", async t => {
     for (let i = 0; i < 50; i++) {
         const txId = await simpleSend("Kzm1oz8sLSvuQLEgu99xixSkhNj7kyBZe6TpqTfdgKGhXUeSLSRx",
             "simpleledger:qp2mdqc4r8ylsrnu9f4j5xu8r47m722f6uyfsz4zr6",
-            "5f5119b1864711025fddfdd75022299b6758dde9eb485b706cda2e40e2ec6641",
+            "323437d4c86b00874c3b00cd454ab6ffb3226130fde09747009cf270caedddcf",
             new BigNumber(Math.floor(Math.random() * 1000) + 100));
         console.log(`RESULT ${txId}`);
         await delay(1000);
